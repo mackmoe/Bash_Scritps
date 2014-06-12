ac9fae71917db2826a9e9dfa9064bf04f4f12f43
mackmoe <maurice.nash1@gmail.com>
2014-06-12 09:53:28 -0500

    Simple Lampstack Deployment
    Targeted OS: Centos 6.5
    Automation of time and security
    Installs: PHP 5.3, MySQL 5.2, PHP MyAdmin 5.1 and Fail2ban

---
+++ ezlampstack.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo -e "Setting the local date and time for the server:\n"; sleep 3;
+cd /etc; ln -sf /usr/share/zoneinfo/Americas/Chicago localtime; echo -e "`date`\n"; sleep 3
+echo -e "Done\n"; sleep 3;
+
+echo -e "Installing all the packages\n" && sleep 3;
+yum install -y mysql mysql-server httpd php php-mysql php-gd php-imap php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-mcrypt php-mssql php-snmp php-soap php-tidy curl curl-devel php-pecl-apc; echo -e "Done\n"; sleep 3;
+
+echo -e "Enableing Serveices on boot\n";
+chkconfig httpd on && chkconfig mysqld on; sleep 3;
+echo -e "Done\n"; sleep 3;
+
+echo -e "Starting Services"
+service mysqld start; service httpd start; sleep 3;
+echo -e "Done\n";
+
+echo -e "Kicking off the MySQL Server Install\n"; sleep 3;
+mysql_secure_installation; echo -e "Done\n";
+
+echo -e "Performing post installation services re-kick for MySQL and Apache\n";
+service mysqld restart; service httpd restart; sleep 3;
+echo -e "Done with your LampStack! Now installing PHPMyAdmin\n"; sleep 3;
+
+echo -e "Setting up the RPMforge Repo for PHPMyAdmin\n"; sleep 3;
+rpm --import http://dag.wieers.com/rpm/packages/RPM-GPG-KEY.dag.txt;
+echo -e "Done\n";
+
+echo -e "Installing PHPMyAdmin\n"
+yum -y install http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm; yum -y install phpmyadmin; echo -e "Go to: http://www.howtoforge.com/apache_php_mysql_on_centos_6.5_lamp and pick up from section 7 - PHPMyAdmin Install\n";

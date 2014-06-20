6a94a489b71f59d5d413efb3e92fd0800e62254f
mackmoe <maurice.nash1@gmail.com>
2014-06-20 01:55:34 -0500

    Fun figlet fo mah login
    C’mon man, you gotta fillet bro…

---
+++ war_cry.sh
@@ -0,0 +1,2 @@
+figlet "! Mo Nash !" -m-5 -f /usr/local/Cellar/figlet/2.2.5/share/figlet/fonts/larry3d.flf;
+figlet "! In bAsH !" -m-5 -f /usr/local/Cellar/figlet/2.2.5/share/figlet/fonts/larry3d.flf
